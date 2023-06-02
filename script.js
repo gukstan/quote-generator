3459d1629c50d49d020bea17aca7888e0adedc97
gukstan <gukstan@gmail.com>
2023-06-02 15:54:43 -0300

    add loader

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+
 // Show New Quote
 function newQuote() {
+    loading();
     //Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if Author field is blank and replace it with 'Unknown'
@@ -17,17 +32,18 @@ function newQuote() {
     else {
         authorText.textContent = quote.author;
     }
-
-    //Check Quote length to determine styling
+      //Check Quote length to determine styling
     if (quote.text.length > 120) {quoteText.classList.add('long-quote');}
     else {quoteText.classList.remove('long-quote');}
-
-
     quoteText.textContent = quote.text;
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
